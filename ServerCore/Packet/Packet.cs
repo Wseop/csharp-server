4e2d5a2d148ae7df79880da76df310a751fb50ef
wooseop <crazyz2000@naver.com>
2024-06-20 17:16:05 +0900

    feat:Enter, Exit, Msg Packet

--- ServerCore/Packet/Packet.cs
+++ ServerCore/Packet/Packet.cs
@@ -4,13 +4,45 @@ namespace ServerCore.Packet
 {
     public enum EPacketType : ushort
     { 
-        Test,
+        Msg,
+        C_Enter,
+        S_Enter,
+        C_Exit,
+        S_Exit,
     }
 
     [ProtoContract]
-    public class PacketTest
+    public class PacketMsg
     {
         [ProtoMember(1)]
         public string Msg { get; set; }
+    }
+
+    [ProtoContract]
+    public class PacketC_Enter
+    {
+        [ProtoMember(1)]
+        public string Name { get; set; }
+    }
+
+    [ProtoContract]
+    public class PacketS_Enter
+    {
+        [ProtoMember(1)]
+        public bool Success { get; set; }
+    }
+
+    [ProtoContract]
+    public class PacketC_Exit
+    {
+        [ProtoMember(1)]
+        public string Name { get; set; }
+    }
+
+    [ProtoContract]
+    public class PacketS_Exit
+    {
+        [ProtoMember(1)]
+        public bool Success { get; set; }
     }
 }

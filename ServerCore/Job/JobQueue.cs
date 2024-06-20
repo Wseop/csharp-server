9cbb41a0fa08c9d50470495895960aba1c9ac3ff
wooseop <crazyz2000@naver.com>
2024-06-20 14:18:48 +0900

    feat:JobQueue, JobTimer

---
+++ ServerCore/Job/JobQueue.cs
@@ -0,0 +1,53 @@
+﻿namespace ServerCore.Job
+{
+    public class JobQueue
+    {
+        object _lock = new object();
+        Queue<Action> _jobs = new Queue<Action>();
+        bool _executing = false;
+
+        public void PushJob(Action job, bool pushOnly = false)
+        {
+            bool execute = false;
+
+            lock (_lock)
+            {
+                _jobs.Enqueue(job);
+
+                // Job을 처리중인 Thread가 없으면 현재 Thread가 처리
+                if (pushOnly == false && _executing == false)
+                {
+                    execute = true;
+                }
+            }
+
+            if (execute)
+            {
+                Execute();
+            }
+        }
+
+        public void Execute()
+        {
+            while (true)
+            {
+                Action job = null;
+
+                lock (_lock)
+                {
+                    // 남은 Job이 없으면 종료
+                    if (_jobs.Count == 0)
+                    {
+                        _executing = false;
+                        break;
+                    }
+
+                    job = _jobs.Dequeue();
+                }
+
+                // lock을 풀고! 실행
+                job();
+            }
+        }
+    }
+}
---
+++ ServerCore/Job/JobTimer.cs
@@ -0,0 +1,57 @@
+﻿namespace ServerCore.Job
+{
+    class ReservedJob
+    {
+        public JobQueue JobQueue { get; set; } = null;
+        public Action Job { get; set; } = null;
+        public int ExecuteTick { get; set; } = 0;
+    }
+
+    public class JobTimer
+    {
+        public static JobTimer Instance { get; } = new JobTimer();
+
+        object _lock = new object();
+        PriorityQueue<ReservedJob, int> _reservedJobs = new PriorityQueue<ReservedJob, int>();
+
+        private JobTimer()
+        { }
+
+        public void ReserveJob(JobQueue jobQueue, Action job, int reserveTick)
+        {
+            ReservedJob reservedJob = new ReservedJob();
+            reservedJob.JobQueue = jobQueue;
+            reservedJob.Job = job;
+            reservedJob.ExecuteTick = System.Environment.TickCount + reserveTick;
+
+            lock (_lock)
+            {
+                _reservedJobs.Enqueue(reservedJob, reservedJob.ExecuteTick);
+            }
+        }
+
+        public void DistributeJobs()
+        {
+            while (true)
+            {
+                int currentTick = System.Environment.TickCount;
+                ReservedJob reservedJob = null;
+
+                lock (_lock)
+                {
+                    if (_reservedJobs.Count == 0)
+                        break;
+
+                    // 아직 실행해야할 Tick이 아니면 종료
+                    if (_reservedJobs.Peek().ExecuteTick > currentTick)
+                        break;
+
+                    reservedJob = _reservedJobs.Dequeue();
+                }
+
+                // 예약된 작업을 해당하는 JobQueue에 Push
+                reservedJob.JobQueue.PushJob(reservedJob.Job, true);
+            }
+        }
+    }
+}
